c7c0138bede949c1e853887fbc0147951943d420
Nilesh Raju Ambekar <nileshambekar9281@gmail.com>
2025-07-26 15:41:55 +0530

    build issues fix

--- marathi-lang/src/components/ui/calendar.tsx
+++ marathi-lang/src/components/ui/calendar.tsx
@@ -1,9 +1,11 @@
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+// import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
+
+
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -52,12 +54,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        // IconLeft: ({ className, ...props }: { className?: string } & React.SVGProps<SVGSVGElement>) => (
+        //   <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+        // ),
+        // IconRight: ({ className, ...props }: { className?: string } & React.SVGProps<SVGSVGElement>) => (
+        //   <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+        // ),
       }}
       {...props}
     />
